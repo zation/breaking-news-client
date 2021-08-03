637c1b720ad7352c95fd0649dbc05d0d86fe85cc
Liu Yang <zation1@gmail.com>
2021-08-03 22:24:53 +0800

    fix header

--- src/shared/components/layout/header.js
+++ src/shared/components/layout/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import useStyles from 'isomorphic-style-loader/useStyles';
-import { Row, Col, Button, Input } from 'antd';
+import { Row, Col, Button, Input, Affix } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
 import s from './header.less';
@@ -9,19 +9,21 @@ const result = () => {
   useStyles(s);
 
   return (
-    <div className={s.Root}>
-      <Row className={s.Content} align="middle">
-        <Col span={5} className={s.LogoContainer}>
-          <div className={s.Logo}>Breaking News</div>
-        </Col>
-        <Col span={14}>
-          <Input className={s.Search} placeholder="搜索爆料" suffix={<SearchOutlined className={s.SearchIcon} />} size="large" />
-        </Col>
-        <Col span={5} className={s.ButtonContainer}>
-          <Button type="primary" size="large" block>添加爆料</Button>
-        </Col>
-      </Row>
-    </div>
+    <Affix offsetTop={0}>
+      <div className={s.Root}>
+        <Row className={s.Content} align="middle">
+          <Col span={5} className={s.LogoContainer}>
+            <div className={s.Logo}>Breaking News</div>
+          </Col>
+          <Col span={14}>
+            <Input className={s.Search} placeholder="搜索爆料" suffix={<SearchOutlined className={s.SearchIcon} />} size="large" />
+          </Col>
+          <Col span={5} className={s.ButtonContainer}>
+            <Button type="primary" size="large" block>添加爆料</Button>
+          </Col>
+        </Row>
+      </div>
+    </Affix>
   );
 };
 
